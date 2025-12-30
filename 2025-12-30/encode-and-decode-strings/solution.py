8ec2a0570c97c2bb4663d2ab7ceb5c40d4e05f68
Ahraz <1kibriaahr@gmail.com>
2025-12-30 09:12:34 -0500

    Added encode-and-decode-strings on 2025-12-30

---
+++ 2025-12-30/encode-and-decode-strings/solution.py
@@ -0,0 +1,34 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        print(strs)
+        sentence = []
+        for words in strs:
+            
+            word = words +'#'
+            # print(word)
+            sentence.append(word)
+            # print(sentence)
+
+        final = ''.join(sentence)
+        print(final)
+        return final
+
+    def decode(self, s: str) -> List[str]:
+        print(s)
+
+        rejoin = [s]
+        print(rejoin)
+        tempword =''
+        res = []
+
+        for char in s:
+            if char != "#":
+                tempword += char
+                print(tempword)
+            else:
+                res.append(tempword)
+                tempword = ''
+                print(res)
+        return res
+

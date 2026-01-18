d93cb014932c388da538ce4f3901bd1d9aeff3cb
Ahraz <1kibriaahr@gmail.com>
2026-01-18 10:21:58 -0500

    Added wrong-answer on 2026-01-18

--- 2026-01-18/wrong-answer/solution.py
+++ 2026-01-18/wrong-answer/solution.py
@@ -1,17 +1,21 @@
-ss Solution:
+class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         hashMap = defaultdict(int)
 
         for char in s:
             hashMap[char] +=1
-        print(hashMap)
+        # print(hashMap)
 
         wordMap = dict(sorted(hashMap.items()))
-        print(wordMap)
+        # print(wordMap)
 
+        sSorted = sorted(s)
+        print(sSorted)
+        tSorted = sorted(t)
+        print(tSorted)
 
         for char in t:
-            if char not in wordMap or len(s) != len(t):
-                print(char)
+            if char not in wordMap or len(s) != len(t)  :
+                # print(char)
                 return False
         return True

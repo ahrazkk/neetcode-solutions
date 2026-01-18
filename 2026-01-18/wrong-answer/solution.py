fe5f9311760f77a2fa030addbebcbd979e255682
Ahraz <1kibriaahr@gmail.com>
2026-01-18 10:27:24 -0500

    Added wrong-answer on 2026-01-18

--- 2026-01-18/wrong-answer/solution.py
+++ 2026-01-18/wrong-answer/solution.py
@@ -1,17 +1,10 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        hashMap = defaultdict(int)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}
 
-        for char in s:
-            hashMap[char] +=1
-        print(hashMap)
-
-        wordMap = dict(sorted(hashMap.items()))
-        print(wordMap)
-
-
-        for char in t:
-            if char not in wordMap:
-                print(char)
-                return False
-        return True
+        
+        for num in nums:
+            goal = target - num
+            print(goal)
+            if goal in nums:
+                return goal
